fd405c78a930a6d4a11c3783f2eebcccc2e37f92
kutukmu <kutukmu@gmail.com>
2022-05-14 17:59:06 -0500

    google tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,5 +38,23 @@ module.exports = {
         icon: "src/Gl-logos.jpeg",
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        enableWebVitalsTracking: true,
+      },
+    }
   ],
 }
